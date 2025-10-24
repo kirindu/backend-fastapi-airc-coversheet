9d8c20a2e76638e4a65e8185586c36f134ab670a
Roberto Payan <kirindu3@gmail.com>
2025-10-24 16:00:02 -0600

    Se corrige el servicio de HomeBase

--- schemas/homebase_scheme.py
+++ schemas/homebase_scheme.py
@@ -2,7 +2,7 @@
 def homebase_helper(homebase) -> dict:
     return {
         "id": str(homebase["_id"]),
-        "homeBaseNumber": homebase["homeBaseNumber"],
+        "homeBaseName": homebase["homeBaseName"],
         "createdAt": homebase["createdAt"].isoformat() if "createdAt" in homebase else None,
         
     }
